c8bf757a4ab89c382016efdb452471171c098c61
Stefan Schintler <stefan@schintler.at>
2023-10-20 00:53:47 +0200

    Reimplement pooling logic to separate WebWorker to avoid blocking the main thread

---
+++ packages/explorer/src/pool/pool-node.ts
@@ -0,0 +1,150 @@
+import { isString } from "lodash";
+
+import wasmFileUrl from "@giz/explorer-backend-libgit2/dist/explorer-backend-libgit2.wasm?url";
+import { WasiRuntime } from "@giz/wasi-runtime";
+
+import { DataResponse, ErrorResponse, JobWithOrigin } from "./types";
+
+/**
+ * A PoolNode represents single worker in the pool, but lives within the pool-master thread
+ */
+export class PoolNode {
+  starting!: Promise<void>;
+
+  runtime?: WasiRuntime;
+  activeJob?: JobWithOrigin;
+
+  get busy() {
+    return !!this.activeJob;
+  }
+
+  get started() {
+    return !!this.runtime;
+  }
+
+  constructor() {}
+
+  boot(handle: FileSystemDirectoryHandle) {
+    this.starting = this._boot(handle);
+  }
+
+  private async _boot(handle: FileSystemDirectoryHandle): Promise<void> {
+    const runtime = await WasiRuntime.create({
+      moduleUrl: wasmFileUrl,
+      moduleName: "/module.wasm",
+      folderMappings: {
+        "/repo": handle,
+      },
+    });
+
+    runtime.run({
+      env: {},
+      args: [],
+    });
+
+    let stdout = "";
+    let maxTries = 100;
+    while (!stdout && maxTries--) {
+      stdout = await runtime.readStdout();
+
+      if (!stdout) {
+        await new Promise((resolve) => setTimeout(resolve, 100));
+      }
+    }
+
+    if (maxTries <= 0) {
+      throw new Error("Timeout waiting for worker to start");
+    }
+
+    const data = JSON.parse(stdout);
+    if (!data?.ready) {
+      throw new Error(data.error);
+    }
+
+    this.runtime = runtime;
+  }
+
+  sendResponse(job: JobWithOrigin, data: any, end?: boolean) {
+    const msg: DataResponse = {
+      id: job.id,
+      data,
+      end,
+    };
+    job.origin.postMessage(msg);
+  }
+
+  sendError(job: JobWithOrigin, error: string | Error | unknown) {
+    let errorMsg = "";
+
+    if (isString(error)) {
+      errorMsg = error;
+    } else if (error instanceof Error) {
+      errorMsg = error.message;
+    } else {
+      errorMsg = JSON.stringify(error);
+    }
+
+    const msg: ErrorResponse = {
+      id: job.id,
+      error: errorMsg,
+      end: true,
+    };
+    job.origin.postMessage(msg);
+  }
+
+  execute(job: JobWithOrigin): Promise<void> {
+    if (this.busy || !this.started || !this.runtime) {
+      throw new Error("PoolNode is busy or not started");
+    }
+
+    this.activeJob = job;
+
+    const { id, method, params } = job;
+
+    const payload = {
+      jsonrpc: "2.0",
+      id,
+      method,
+      params,
+    };
+
+    const payloadString = JSON.stringify(payload) + "\n";
+    this.runtime!.writeStdin(payloadString);
+
+    return this.handleResponse(job);
+  }
+
+  async handleResponse(job: JobWithOrigin) {
+    while (this.busy) {
+      const stdout = await this.runtime!.readStdout();
+
+      let data;
+      try {
+        data = JSON.parse(stdout);
+      } catch (error) {
+        this.sendError(job, error);
+        console.error("error parsing stdout", stdout, error);
+        break;
+      }
+
+      const isIntermediateResponse = !isString(data?.jsonrpc);
+      //this.logger.trace("stdout", stdout);
+
+      if (data.error) {
+        this.sendError(job, data.error.message);
+        break;
+      }
+      if (!isIntermediateResponse) {
+        this.sendResponse(job, data.result, true);
+        break;
+      }
+
+      this.sendResponse(job, data);
+    }
+    this.activeJob = undefined;
+  }
+
+  dispose() {
+    this.runtime?.dispose();
+  }
+}
---
+++ packages/explorer/src/pool/pool-portal.ts
@@ -0,0 +1,268 @@
+import { Author, Blame, FileTreeNode, GitGraph } from "../types";
+
+import { PoolResponse, PoolTask } from "./types";
+
+export type PortaledJob = {
+  id: number;
+  priority: number;
+  method: string;
+  params: any[];
+  onEnd: (data: any) => void;
+  onErr: (err: any) => void;
+  onData?: (data: any) => void;
+};
+export type PortaledJobOpts = {
+  id?: number;
+  priority?: number;
+  method: string;
+  params: any[];
+  onEnd: (data: any) => void;
+  onErr: (err: any) => void;
+  onData?: (data: any) => void;
+};
+
+export class JobRef<T> {
+  private id_: number;
+  private priority_: number;
+  private promise_: Promise<T>;
+  constructor(
+    private portal: PoolPortal,
+    job: PortaledJob,
+  ) {
+    this.id_ = job.id;
+    this.priority_ = job.priority ?? 1;
+
+    this.promise_ = new Promise<T>((resolve, reject) => {
+      const originalOnEnd = job.onEnd;
+      const originalOnErr = job.onErr;
+
+      job.onEnd = (data) => {
+        originalOnEnd(data);
+        resolve(data);
+      };
+      job.onErr = (error) => {
+        originalOnErr(error);
+        reject(error);
+      };
+    });
+  }
+
+  get priority() {
+    return this.priority_;
+  }
+
+  setPriority(priority: number) {
+    this.priority_ = priority;
+    this.portal.setJobPriority(this.id_, priority);
+  }
+
+  cancel() {
+    console.warn("cancel", this.id_);
+    this.portal.cancelJob(this.id_);
+  }
+
+  get promise() {
+    return this.promise_;
+  }
+}
+
+export class PoolPortal {
+  private counter = 0;
+  private jobs: PortaledJob[] = [];
+
+  constructor(private port: MessagePort) {
+    this.onMessage = this.onMessage.bind(this);
+    this.onMessageError = this.onMessageError.bind(this);
+
+    port.onmessage = this.onMessage;
+    port.onmessageerror = this.onMessageError;
+  }
+
+  dispose() {
+    this.sendTask({
+      type: "close",
+    });
+
+    this.port.close();
+
+    for (const j of this.jobs) {
+      j.onErr("Pool closed");
+    }
+
+    this.jobs = [];
+  }
+
+  onMessage(message: MessageEvent<PoolResponse>) {
+    const data: PoolResponse = message.data;
+    const jobId = data.id;
+
+    const job = this.jobs.find((j) => j.id === jobId);
+
+    if (!job) {
+      console.warn("Job not found", jobId);
+      return;
+    }
+
+    if (!data.end) {
+      job.onData?.(data.data);
+      return;
+    }
+
+    if (message.data.end) {
+      this.removeJob(jobId);
+    }
+
+    if ("error" in data) {
+      job.onErr(data.error);
+      return;
+    }
+
+    if ("data" in data) {
+      job.onEnd(data.data);
+    }
+  }
+
+  removeJob(jobId: number) {
+    const index = this.jobs.findIndex((j) => j.id === jobId);
+    if (index !== -1) {
+      this.jobs.splice(index, 1);
+    }
+  }
+
+  onMessageError(message: MessageEvent<unknown>) {
+    console.error("Port message error", message);
+  }
+
+  execute<T>(method: string, params?: any[], priority = 100): JobRef<T> {
+    const job = {
+      id: this.counter++,
+      priority,
+      params: params ?? [],
+      method,
+      onEnd: () => {},
+      onErr: () => {},
+    };
+
+    const ref = new JobRef<T>(this, job);
+
+    this._enqueueJob(job);
+
+    return ref;
+  }
+
+  stream(opts: PortaledJobOpts) {
+    this._enqueueJob({
+      ...opts,
+      id: opts.id ?? this.counter++,
+      priority: opts.priority ?? 1,
+    });
+  }
+
+  sendTask(task: PoolTask) {
+    this.port.postMessage(task);
+  }
+
+  cancelJob(id: number) {
+    const job = this.jobs.find((job) => job.id === id);
+    if (!job) {
+      return;
+    }
+
+    this.sendTask({
+      type: "remove",
+      jobId: job.id,
+    });
+
+    job.onErr(new Error("Job cancelled"));
+
+    this.removeJob(id);
+  }
+
+  setJobPriority(id: number, priority: number) {
+    const job = this.jobs.find((job) => job.id === id);
+    if (!job) {
+      return;
+    }
+    job.priority = priority;
+    this.sendTask({
+      type: "update",
+      jobId: job.id,
+      priority,
+    });
+  }
+
+  _enqueueJob(job_: PortaledJob) {
+    const id = job_.id ?? this.counter++;
+    const priority = job_.priority ?? 1;
+
+    const job = {
+      ...job_,
+      id,
+      priority,
+    };
+
+    this.jobs.push(job);
+
+    const ref = new JobRef(this, job);
+
+    this.sendTask({
+      type: "new",
+      job: {
+        id,
+        priority,
+        method: job.method,
+        params: job.params,
+      },
+    });
+
+    return ref;
+  }
+
+  setPriority(jobId: number, priority: number) {
+    const job = this.jobs.find((j) => j.id === jobId);
+    if (job) {
+      job.priority = priority;
+    }
+  }
+
+  getBranches(): Promise<string[]> {
+    return this.execute<string[]>("list_branches").promise;
+  }
+
+  getBlame(branch: string, path: string, preview?: boolean): JobRef<Blame> {
+    return this.execute<Blame>("blame", [{ branch, path, preview }], preview ? 100 : 1);
+  }
+
+  getFileContent(branch: string, path: string): Promise<string> {
+    return this.execute<string>("file_content", [{ branch, path }]).promise;
+  }
+
+  getGitGraph() {
+    return this.execute<GitGraph>("git_graph").promise;
+  }
+
+  streamFileTree(
+    branch: string,
+    onData: (data: FileTreeNode) => void,
+    onEnd: () => void,
+    onErr: (err: any) => void,
+  ) {
+    return this.stream({
+      method: "file_tree",
+      params: [{ branch }],
+      onData,
+      onEnd,
+      onErr,
+    });
+  }
+
+  streamAuthors(onData: (data: Author) => void, onEnd: () => void, onErr: (err: any) => void) {
+    return this.stream({
+      method: "stream_authors",
+      params: [],
+      onData,
+      onEnd,
+      onErr,
+    });
+  }
+}
