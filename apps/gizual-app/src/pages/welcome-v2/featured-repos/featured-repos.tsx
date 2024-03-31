839d7e3e6406a195a6305696dc5fc49b95b7637a
Stefan Schintler <stefan@schintler.at>
2024-03-31 15:14:43 +0200

    Hide featured repo stats for now

--- apps/gizual-app/src/pages/welcome-v2/featured-repos/featured-repos.tsx
+++ apps/gizual-app/src/pages/welcome-v2/featured-repos/featured-repos.tsx
@@ -7,24 +7,15 @@ function FeaturedRepos() {
       <RepoCard
         repoName="tugraz-isds/diamond"
         repoSource="https://github.com/tugraz-isds/diamond"
-        metrics={{ stars: 2 }}
       />
       <RepoCard repoName="ts-rest/ts-rest" repoSource="https://github.com/ts-rest/ts-rest" />
       <RepoCard repoName="pmndrs/zustand" repoSource="https://github.com/pmndrs/zustand" />
       <RepoCard repoName="mobxjs/mobx" repoSource="https://github.com/mobxjs/mobx" />
-      <RepoCard
-        repoName="vitejs/vite"
-        repoSource="https://github.com/vitejs/vite"
-        metrics={{ stars: 64_000 }}
-      />
+      <RepoCard repoName="vitejs/vite" repoSource="https://github.com/vitejs/vite" />
       <RepoCard repoName="vuejs/vue" repoSource="https://github.com/vuejs/vue" />
       <RepoCard repoName="flutter/flutter" repoSource="https://github.com/flutter/flutter" />
       <RepoCard repoName="facebook/react" repoSource="https://github.com/facebook/react" />
-      <RepoCard
-        repoName="microsoft/vscode"
-        repoSource="https://github.com/microsoft/vscode"
-        metrics={{ stars: 157_000 }}
-      />
+      <RepoCard repoName="microsoft/vscode" repoSource="https://github.com/microsoft/vscode" />
     </div>
   );
 }
